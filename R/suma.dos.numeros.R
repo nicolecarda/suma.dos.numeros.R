103a13a7a876c72f98c1c192db788c95a7f536fd
Nicole Carda Andretto <Ni-ki_96@hotmail.com>
2024-07-30 19:45:24 -0300

    first commit

---
+++ R/suma.dos.numeros.R
@@ -0,0 +1,12 @@
+#' Suma dos números
+#'
+#' Esta función toma dos números y devuelve su suma.
+#'
+#' @param x Un número. El primer número a sumar.
+#' @param y Un número. El segundo número a sumar.
+#' @return La suma de los dos números.
+#' @export
+
+suma.dos.numeros <- function(x,y){
+  return(x+y)
+}
